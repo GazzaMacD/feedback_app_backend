f276ed26767cf49c6c63a75f94c31c3813aaf6ea
Gareth Macdonald <macdonald.gareth@gmail.com>
2021-08-03 12:22:18 +0200

    set up linting and prettier, remove dummy route

--- server.js
+++ server.js
@@ -1,9 +1,6 @@
-const express = require("express");
-const app = express();
+const express = require('express')
+const passport = require('passport')
+const app = express()
 
-app.get("/", (req, res) => {
-    res.send({ hi: "there" });
-});
-
-const PORT = process.env.PORT || 5000;
-app.listen(PORT);
+const PORT = process.env.PORT || 5000
+app.listen(PORT)
