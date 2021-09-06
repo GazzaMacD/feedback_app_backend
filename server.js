66efe3cbb483db516659afe5ed45e35671e42ae4
Gareth Macdonald <macdonald.gareth@gmail.com>
2021-09-06 04:37:57 +0200

    add routes directory so as to split routes

--- server.js
+++ server.js
@@ -1,33 +1,12 @@
+// initialize the passport config
+require('./services/passport.js')
 const express = require('express')
-const passport = require('passport')
-
-const GoogleStrategy = require('passport-google-oauth20').Strategy
-const keys = require('./config/keys.js')
 const app = express()
 
-// Google auth
-passport.use(
-    new GoogleStrategy(
-        {
-            clientID: keys.googleClientID,
-            clientSecret: keys.googleClientSecret,
-            callbackURL: '/auth/google/callback',
-        },
-        (accessToken, refreshToken, profile, done) => {
-            console.log(accessToken)
-            console.log(refreshToken)
-            console.log(profile)
-        }
-    )
-)
+// Route imports
+const authRoutes = require('./routes/authRoutes.js')
 
-app.get(
-    '/auth/google',
-    passport.authenticate('google', {
-        scope: ['profile', 'email'],
-    })
-)
-app.get('/auth/google/callback', passport.authenticate('google'))
+authRoutes(app)
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT)
